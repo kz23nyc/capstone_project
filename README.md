624b2e2327357123c3e7d1b2da58889cc9a1b577
kz23nyc <khinezar.mh@gmail.com>
2024-10-24 21:11:55 -0400

    backend

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# capstone_project
-# capstone_project-backend
-# capstone_project
+# Capstone Backend Project
+
+## Description
+Thanks for exploring the Best-README-Template! If you have any suggestions to improve it, feel free to fork the repository and submit a pull request, or open an issue with the "enhancement" tag. And don’t forget to give the project a star! We really appreciate your support. Now go build something incredible! 😄
+
+## Technolgoies
+
+1. Express
+2. Mongoose
+3. dotenv
+
+## Acknowledgement
--- index.js
+++ index.js
@@ -3,22 +3,38 @@ import morgan from 'morgan';
 import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// =========== Connect to DB ===== //
+try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`Connected to mongodb`);    
+} catch (error) {
+    console.error(error);
+}
+
+// =========== Middleware ===== //
+app.use(morgan('dev')); // logger for development
+app.use(express.json()); // parse incoming JSON request bodies
+app.use(express.urlencoded({extended:true})); // Parses URL-encoded  bodies
+app.use(cors()); //Enable Cross-Origin Resource Sharing- allow backend to talk to frontend in the same machine
+
+//============ Routes ======== //
+app.use('/', projectRouter); //use the recipe router
 
 app.get('/', (req,res) => {
     res.send('Welcome to my Pantrylicious recipe API!')
 });
 
+// =========== Error Middleware ===== //
+app.use((e, req, res, next) => {
+    console.error(e);
+    res.status(500).json({message: e.message, error:e})
+});
+
+
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-
-// =========== Middleware ===== //
-
-//Enable CORS for all requests
-app.use(cors());
-
-// Log all requests using morgan with the 'dev' format
-app.use(morgan('dev')); // logger
